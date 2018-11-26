6caefbddbe393c7665f954949a77a8ea2454c555
Polgár András <azazdeaz@gmail.com>
2018-11-26 22:38:40 +0100

    :sparkles: init basic app with dis-gui

---
+++ packages/apriltag-renderer-app/src/App.tsx
@@ -0,0 +1,33 @@
+import { GUI, Text } from 'dis-gui'
+import * as React from 'react'
+import './App.css'
+
+import logo from './logo.svg'
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <GUI>
+          <Text
+            label="Text"
+            value="Hello world!"
+            onChange={this.onTextChange}
+          />
+        </GUI>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </div>
+    )
+  }
+  private onTextChange = value => {
+    throw new Error(value)
+  }
+}
+
+export default App
